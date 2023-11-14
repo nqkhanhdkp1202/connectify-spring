772965e277e2dd1e4c327aa839819b536bab9c29
Khanh <khanhlp315@gmail.com>
2023-11-15 03:48:56 +0700

    feat: Use lambda for UserDetailsService

--- src/main/java/com/klgroup/connectify/service/impl/UserServiceImpl.java
+++ src/main/java/com/klgroup/connectify/service/impl/UserServiceImpl.java
@@ -3,7 +3,6 @@ package com.klgroup.connectify.service.impl;
 import com.klgroup.connectify.repository.BaseUserRepository;
 import com.klgroup.connectify.service.UserService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
@@ -14,12 +13,7 @@ public class UserServiceImpl implements UserService {
     private final BaseUserRepository userRepository;
     @Override
     public UserDetailsService userDetailsService() {
-        return new UserDetailsService() {
-            @Override
-            public UserDetails loadUserByUsername(String username) {
-                return userRepository.findByUsername(username)
-                        .orElseThrow(() -> new UsernameNotFoundException("User not found"));
-            }
-        };
+        return username -> userRepository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
     }
 }
